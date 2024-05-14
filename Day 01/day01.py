358f4d3083184e2fde91eac7658a3b91963e9659
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2024-05-14 16:31:43 +0500

    Day 01 code + project

---
+++ Day 01/day01.py
@@ -0,0 +1,42 @@
+# Hi This is Badar abbas and iam starting to learn python
+# Day 01
+
+
+# Print Function
+print("Hello World")
+
+# Auditorium Exersice
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+# String manupulation
+print("Hello World\nHello World\nHello World")
+print("Hello" + " " + "World")
+# Input Function
+input("What is your name?")
+# Input Exersice
+print("Hello " + input("What is your name?"))
+# length
+print(len(input("What is your name?")))
+# Variables
+name = input("What is your name?")
+print(name)
+# Variables Exersice
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+
+a = input("a:")
+b = input("b:")
+c = a
+a = b
+b = c
+print("a = " + a)
+print("b = " + b)
+# Band Name Generator
+print("Welocome to the band name genrator")
+city = input("What is the name of the city you BORN?\n ")
+pet = input("What is the name of your pet?\n ")
+print("Your band name is " + city + " " + pet)
