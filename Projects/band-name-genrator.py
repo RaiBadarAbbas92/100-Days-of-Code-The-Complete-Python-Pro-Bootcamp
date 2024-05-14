e9df8e8c8444da535e2dab2123dbc468647aa8e5
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2024-05-14 16:16:12 +0500

    100 Projects of Python

---
+++ Projects/band-name-genrator.py
@@ -0,0 +1,7 @@
+print("Welocome to the band name genrator")
+
+city = input("What is the name of the city you BORN?\n ")
+
+pet = input("What is the name of your pet?\n ")
+
+print("Your band name is " + city + " " + pet)
